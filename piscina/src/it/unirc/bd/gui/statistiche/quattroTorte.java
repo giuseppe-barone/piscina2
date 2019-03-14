bb86a73e826f3e5e736ef0add4ad197f9445b67c
ziolele97 <45795297+ziolele97@users.noreply.github.com>
2019-03-14 20:04:11 +0100

    prova

--- piscina/src/it/unirc/bd/gui/statistiche/quattroTorte.java
+++ piscina/src/it/unirc/bd/gui/statistiche/quattroTorte.java
@@ -1,8 +1,17 @@
 package it.unirc.bd.gui.statistiche;
 
 import java.awt.EventQueue;
+import java.awt.EventQueue; 
+import javax.swing.Icon;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;  
+import java.io.File;
+import java.io.IOException;
 
+import java.awt.Color; import javax.swing.JLabel; 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 public class quattroTorte {
 
@@ -36,8 +45,17 @@ public class quattroTorte {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 834, 621);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*DefaultPieDataset data1 = new DefaultPieDataset();
+		DefaultPieDataset data2 = new DefaultPieDataset();
+		DefaultPieDataset data3 = new DefaultPieDataset();
+		DefaultPieDataset data4 = new DefaultPieDataset();*/
+		JFreeChart chart = new JFreeChart.
+		 ChartPanel panel = new ChartPanel(chart);
+		 panel.setBorder(new LineBorder(new Color(0, 0, 0), 3, true)); 
+		 panel.setBounds(10, 11, 400, 282);
+		 frame.getContentPane().add(panel, BorderLayout.EAST); 
 	}
 
 }
