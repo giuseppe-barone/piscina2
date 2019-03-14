9e69c12ce687c9d2f0a299b464ad222f29903ed2
ziolele97 <45795297+ziolele97@users.noreply.github.com>
2019-03-14 17:49:37 +0100

    aggiornamento statistiche

--- piscina/src/it/unirc/bd/gui/statistiche/statisticheAtleti.java
+++ piscina/src/it/unirc/bd/gui/statistiche/statisticheAtleti.java
@@ -25,6 +25,10 @@ import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import javax.swing.JPasswordField;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JLabel;
 
 public class statisticheAtleti extends JDialog {
 	private IscrittoDAOP iscrittoDAOP=new IscrittoDAOP();
@@ -51,20 +55,16 @@ public class statisticheAtleti extends JDialog {
 	public statisticheAtleti() {
 		
 		ButtonGroup gruppo = new ButtonGroup();		
-		setBounds(100, 100, 584, 409);
+		setBounds(100, 100, 584, 442);
 		getContentPane().setLayout(null);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane.setBounds(0, 0, 566, 365);
+		tabbedPane.setBounds(0, 0, 566, 390);
 		getContentPane().add(tabbedPane);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Infortunii", null, panel, null);
 		panel.setLayout(null);
-		
-		JButton btnNewButton = new JButton("Avvia");
-		btnNewButton.setBounds(225, 300, 97, 25);
-		panel.add(btnNewButton);
 		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBorder(new TitledBorder(null, "Assoluti", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -127,88 +127,199 @@ public class statisticheAtleti extends JDialog {
 		panel_2.add(comboBox_3);
 		
 		JPanel panelConfronti = new JPanel();
-		panelConfronti.setBorder(new TitledBorder(null, "Confronti", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panelConfronti.setBounds(35, 141, 514, 144);
+		panelConfronti.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Atleti", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelConfronti.setBounds(55, 125, 406, 193);
 		panel.add(panelConfronti);
 		panelConfronti.setLayout(null);
 		
 		JComboBox<Iscritto> comboAtleta1 = new JComboBox<Iscritto>();
 		comboAtleta1.setModel(iscrittoDAOP.getAtleticb());
 		comboAtleta1.setEnabled(false);
-		comboAtleta1.setBounds(63, 23, 314, 22);
+		comboAtleta1.setBounds(63, 56, 314, 22);
 		panelConfronti.add(comboAtleta1);
-		
-		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
-		comboBox_5.setEnabled(false);
-		comboBox_5.setBounds(400, 23, 102, 22);
-		panelConfronti.add(comboBox_5);
 		
 		JCheckBox checkBox_1 = new JCheckBox("1\u00B0");
 		checkBox_1.setEnabled(false);
-		checkBox_1.setBounds(8, 22, 41, 25);
+		checkBox_1.setBounds(8, 55, 41, 25);
 		panelConfronti.add(checkBox_1);
 		
 		JCheckBox checkBox_2 = new JCheckBox("2\u00B0");
 		checkBox_2.setEnabled(false);
-		checkBox_2.setBounds(8, 52, 41, 25);
+		checkBox_2.setBounds(8, 85, 41, 25);
 		panelConfronti.add(checkBox_2);
 		
 		JComboBox<Iscritto> comboAtleta2 = new JComboBox<Iscritto>();
 		comboAtleta2.setModel(iscrittoDAOP.getAtleticb());
 		comboAtleta2.setEnabled(false);
-		comboAtleta2.setBounds(63, 53, 314, 22);
+		comboAtleta2.setBounds(63, 86, 314, 22);
 		panelConfronti.add(comboAtleta2);
-		
-		JComboBox comboBox_7 = new JComboBox();
-		comboBox_7.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
-		comboBox_7.setEnabled(false);
-		comboBox_7.setBounds(400, 53, 102, 22);
-		panelConfronti.add(comboBox_7);
 		
 		JCheckBox checkBox_3 = new JCheckBox("4\u00B0");
 		checkBox_3.setEnabled(false);
-		checkBox_3.setBounds(8, 112, 41, 25);
+		checkBox_3.setBounds(8, 145, 41, 25);
 		panelConfronti.add(checkBox_3);
 		
 		JComboBox<Iscritto> comboAtleta4 = new JComboBox<Iscritto>();
 		comboAtleta4.setModel(iscrittoDAOP.getAtleticb());
 		comboAtleta4.setEnabled(false);
-		comboAtleta4.setBounds(63, 113, 314, 22);
+		comboAtleta4.setBounds(63, 146, 314, 22);
 		panelConfronti.add(comboAtleta4);
-		
-		JComboBox comboBox_9 = new JComboBox();
-		comboBox_9.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
-		comboBox_9.setEnabled(false);
-		comboBox_9.setBounds(400, 113, 102, 22);
-		panelConfronti.add(comboBox_9);
-		
-		JComboBox comboBox_10 = new JComboBox();
-		comboBox_10.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
-		comboBox_10.setEnabled(false);
-		comboBox_10.setBounds(400, 83, 102, 22);
-		panelConfronti.add(comboBox_10);
 		
 		JComboBox<Iscritto> comboAtleta3 = new JComboBox<Iscritto>();
 		comboAtleta3.setModel(iscrittoDAOP.getAtleticb());
 		comboAtleta3.setEnabled(false);
-		comboAtleta3.setBounds(63, 83, 314, 22);
+		comboAtleta3.setBounds(63, 116, 314, 22);
 		panelConfronti.add(comboAtleta3);
 		
 		JCheckBox checkBox_4 = new JCheckBox("3\u00B0");
 		checkBox_4.setEnabled(false);
-		checkBox_4.setBounds(8, 82, 41, 25);
+		checkBox_4.setBounds(8, 115, 41, 25);
 		panelConfronti.add(checkBox_4);
 		
+		JLabel lblPeriodo = new JLabel("Anno:");
+		lblPeriodo.setEnabled(false);
+		lblPeriodo.setBounds(63, 27, 41, 16);
+		panelConfronti.add(lblPeriodo);
+		
+		JYearChooser yearChooser_2 = new JYearChooser();
+		yearChooser_2.setStartYear(2000);
+		yearChooser_2.setMinimum(2000);
+		yearChooser_2.setBounds(103, 21, 51, 22);
+		panelConfronti.add(yearChooser_2);
+		
 		JRadioButton radioConfronti = new JRadioButton("");
-		radioConfronti.setBounds(12, 141, 25, 25);
+		radioConfronti.setBounds(22, 121, 25, 25);
 		panel.add(radioConfronti);
 		gruppo.add(radioConfronti);
+		
+		JButton btnNewButton = new JButton("Avvia");
+		btnNewButton.setBounds(209, 320, 97, 25);
+		panel.add(btnNewButton);
 		
 		
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Eventi", null, panel_1, null);
+		panel_1.setLayout(null);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setLayout(null);
+		panel_3.setBorder(new TitledBorder(null, "Assoluti", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_3.setBounds(12, 13, 537, 99);
+		panel_1.add(panel_3);
+		
+		JRadioButton rdbtnVittorieAssociatiAd = new JRadioButton("Vittorie associati ad allenatori");
+		rdbtnVittorieAssociatiAd.setBounds(8, 26, 205, 25);
+		panel_3.add(rdbtnVittorieAssociatiAd);
+		
+		JCheckBox checkBox_5 = new JCheckBox("Anno");
+		checkBox_5.setEnabled(false);
+		checkBox_5.setBounds(217, 26, 57, 25);
+		panel_3.add(checkBox_5);
+		
+		JYearChooser yearChooser_3 = new JYearChooser();
+		yearChooser_3.getSpinner().setEnabled(false);
+		yearChooser_3.setBounds(274, 29, 51, 22);
+		panel_3.add(yearChooser_3);
+		
+		JComboBox comboBox_4 = new JComboBox();
+		comboBox_4.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
+		comboBox_4.setEnabled(false);
+		comboBox_4.setBounds(337, 27, 78, 22);
+		panel_3.add(comboBox_4);
+		
+		JRadioButton rdbtnVittorieAssociatiA = new JRadioButton("Vittorie associati a corsi");
+		rdbtnVittorieAssociatiA.setBounds(8, 56, 205, 25);
+		panel_3.add(rdbtnVittorieAssociatiA);
+		
+		JCheckBox checkBox_6 = new JCheckBox("Anno");
+		checkBox_6.setEnabled(false);
+		checkBox_6.setBounds(217, 56, 57, 25);
+		panel_3.add(checkBox_6);
+		
+		JYearChooser yearChooser_4 = new JYearChooser();
+		yearChooser_4.getSpinner().setEnabled(false);
+		yearChooser_4.setBounds(274, 59, 51, 22);
+		panel_3.add(yearChooser_4);
+		
+		JComboBox comboBox_5 = new JComboBox();
+		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"Qualsiasi", "Lieve", "Media", "Elevata"}));
+		comboBox_5.setEnabled(false);
+		comboBox_5.setBounds(337, 57, 78, 22);
+		panel_3.add(comboBox_5);
+		
+		JComboBox comboBox_6 = new JComboBox();
+		comboBox_6.setModel(new DefaultComboBoxModel(new String[] {"Podio", "Primo", "Secondo", "Terzo"}));
+		comboBox_6.setEnabled(false);
+		comboBox_6.setBounds(427, 27, 78, 22);
+		panel_3.add(comboBox_6);
+		
+		JComboBox comboBox_7 = new JComboBox();
+		comboBox_7.setModel(new DefaultComboBoxModel(new String[] {"Podio", "Primo", "Secondo", "Terzo"}));
+		comboBox_7.setEnabled(false);
+		comboBox_7.setBounds(427, 57, 78, 22);
+		panel_3.add(comboBox_7);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setLayout(null);
+		panel_4.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Atleti", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_4.setBounds(37, 128, 406, 193);
+		panel_1.add(panel_4);
+		
+		JComboBox<Iscritto> comboBox_8 = new JComboBox<Iscritto>();
+		comboBox_8.setEnabled(false);
+		comboBox_8.setBounds(63, 56, 314, 22);
+		panel_4.add(comboBox_8);
+		
+		JCheckBox checkBox_7 = new JCheckBox("1\u00B0");
+		checkBox_7.setEnabled(false);
+		checkBox_7.setBounds(8, 55, 41, 25);
+		panel_4.add(checkBox_7);
+		
+		JCheckBox checkBox_8 = new JCheckBox("2\u00B0");
+		checkBox_8.setEnabled(false);
+		checkBox_8.setBounds(8, 85, 41, 25);
+		panel_4.add(checkBox_8);
+		
+		JComboBox<Iscritto> comboBox_9 = new JComboBox<Iscritto>();
+		comboBox_9.setEnabled(false);
+		comboBox_9.setBounds(63, 86, 314, 22);
+		panel_4.add(comboBox_9);
+		
+		JCheckBox checkBox_9 = new JCheckBox("4\u00B0");
+		checkBox_9.setEnabled(false);
+		checkBox_9.setBounds(8, 145, 41, 25);
+		panel_4.add(checkBox_9);
+		
+		JComboBox<Iscritto> comboBox_10 = new JComboBox<Iscritto>();
+		comboBox_10.setEnabled(false);
+		comboBox_10.setBounds(63, 146, 314, 22);
+		panel_4.add(comboBox_10);
+		
+		JComboBox<Iscritto> comboBox_11 = new JComboBox<Iscritto>();
+		comboBox_11.setEnabled(false);
+		comboBox_11.setBounds(63, 116, 314, 22);
+		panel_4.add(comboBox_11);
+		
+		JCheckBox checkBox_10 = new JCheckBox("3\u00B0");
+		checkBox_10.setEnabled(false);
+		checkBox_10.setBounds(8, 115, 41, 25);
+		panel_4.add(checkBox_10);
+		
+		JLabel label = new JLabel("Anno:");
+		label.setEnabled(false);
+		label.setBounds(63, 27, 41, 16);
+		panel_4.add(label);
+		
+		JYearChooser yearChooser_5 = new JYearChooser();
+		yearChooser_5.setStartYear(2000);
+		yearChooser_5.setMinimum(2000);
+		yearChooser_5.setBounds(103, 21, 51, 22);
+		panel_4.add(yearChooser_5);
+		
+		JRadioButton radioButton = new JRadioButton("");
+		radioButton.setBounds(12, 121, 25, 25);
+		panel_1.add(radioButton);
 		
 		
 		//----LISTNER----
